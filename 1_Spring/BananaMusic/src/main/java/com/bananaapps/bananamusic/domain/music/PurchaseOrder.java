5e50747b8bd8e8477d99c9d2c4d4713e118deb4f
ricardonetmind <ricardoag@expert.netmind.es>
2024-01-28 22:29:51 +0100

    update

--- 1_Spring/BananaMusic/src/main/java/com/bananaapps/bananamusic/domain/music/PurchaseOrder.java
+++ 1_Spring/BananaMusic/src/main/java/com/bananaapps/bananamusic/domain/music/PurchaseOrder.java
@@ -25,7 +25,7 @@ public class PurchaseOrder {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PurchaseOrderLineSong> lineSongs;
 
 

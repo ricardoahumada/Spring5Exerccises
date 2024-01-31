bfce6d5b833cb9b95be163d78414e488040ac831
ricardonetmind <ricardoag@expert.netmind.es>
2024-01-31 11:05:35 +0100

    update

---
+++ 5_Seguridad/OAuth/oauth2-api-client/src/main/java/com/microcompany/oauth2apiclient/controller/CartController.java
@@ -0,0 +1,30 @@
+package com.microcompany.oauth2apiclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
+
+@RestController
+public class CartController {
+    @Autowired
+    private WebClient webClient;
+
+    @GetMapping(value = "/balance-view")
+    public Double getBalance(
+            @RegisteredOAuth2AuthorizedClient("music-client-authorization-code") OAuth2AuthorizedClient authorizedClient
+    ) {
+        return this.webClient
+                .get()
+                .uri("http://127.0.0.1:9090/cart/balance")
+                .attributes(oauth2AuthorizedClient(authorizedClient))
+                .retrieve()
+                .bodyToMono(Double.class)
+                .block();
+
+    }
+}
